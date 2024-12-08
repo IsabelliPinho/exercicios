6116d975f5a7b6938d1f2e7fddff715de6df984a
Isabelli Araújo Pinho <108484363+IsabelliPinho@users.noreply.github.com>
2024-12-07 22:06:09 -0300

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,35 @@
+/*
+import java.util.Scanner; 
+
+public class Main{
+public static void main (String[]args){
+  Scanner scanner = new Scanner(System.in);
+  
+  System.out.println("Digite 1 num: ");
+  int num1 = scanner.nextInt();
+  System.out.println("Digite 1 num: ");
+  int num2 = scanner.nextInt();
+  System.out.println("Digite 1 num: ");
+  int num3 = scanner.nextInt();
+  System.out.println("Digite 1 num: ");
+  int num4 = scanner.nextInt();
+  System.out.println("Digite 1 num: ");
+  int num5 = scanner.nextInt();
+  
+  int [] array = new int[5];
+  array[0] = num1;
+  array[1] = num2;
+  array[2] = num3;
+  array[3] = num4;
+  array[4] = num5;
+  
+  int soma = 0;
+  
+  for(int i =0; i<array.length; i++){
+    soma = soma += i;
+    System.out.println("Index: "+i+ " valores: "+ array[i]);
+        System.out.println("Soma: "+soma);
+
+  }
+}}
+*/
